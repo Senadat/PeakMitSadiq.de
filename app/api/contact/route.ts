d8ce72993559cd8f18a89fa1a57a2261b2daf0f8
Senadat <kontakt@senadat.de>
2026-01-27 23:18:38 +0100

    Update route.ts

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,6 +1,6 @@
 import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
-const resendEmail = process.env.RESEND_EMAIL ?? "info@peakmitadiq.de"; //
+const resendEmail = process.env.RESEND_EMAIL ?? "info@peakmitsadiq.de"; //
 
 export async function POST(req: Request) {
   try {
